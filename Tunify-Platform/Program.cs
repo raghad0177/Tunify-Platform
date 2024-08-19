6ffc593822d2403351c2c01c633bc3d4dec339ff
Raghad A. Al-yakhri <abdullahraghad616@gmail.com>
2024-08-19 19:46:09 +0300

    done

--- Tunify-Platform/Program.cs
+++ Tunify-Platform/Program.cs
@@ -12,15 +12,39 @@ namespace Tunify_Platform
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Tunify API",
+                    Version = "v1",
+                    Description = "API for managing playlists, songs, and artists in the Tunify Platform"
+                });
+            });
             // Get the connection string settings  
             string ConnectionStringVar = builder.Configuration.GetConnectionString("DefaultConnection");
-
             builder.Services.AddDbContext<TunifyDbContext>(optionsX => optionsX.UseSqlServer(ConnectionStringVar));
             builder.Services.AddScoped<IArtists, ArtistsServices>();
             builder.Services.AddScoped<IPlaylists, PlaylistsServices>();
-            builder.Services.AddScoped<ISongs, SongsServices>(); 
+            builder.Services.AddScoped<ISongs, SongsServices>();
             builder.Services.AddScoped<IUsers, UsersServices>();
             var app = builder.Build();
+            app.UseSwagger(
+             options =>
+             {
+                 options.RouteTemplate = "api/{documentName}/swagger.json";
+             }
+             );
+            //=====
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Index}/{id?}");
+            //=====
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/api/v1/swagger.json", "Tunify API v1");
+                options.RoutePrefix = "";
+            });
             app.MapControllers();
             app.MapGet("/", () => "Hello World!");
             app.Run();
