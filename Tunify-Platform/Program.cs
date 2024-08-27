21fad21e1f86caaff9aa61689cb3a48c6485f701
Raghad A. Al-yakhri <abdullahraghad616@gmail.com>
2024-08-28 00:56:06 +0300

    done

--- Tunify-Platform/Program.cs
+++ Tunify-Platform/Program.cs
@@ -9,7 +9,7 @@ namespace Tunify_Platform
     public class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllers();
             builder.Services.AddSwaggerGen(options =>
